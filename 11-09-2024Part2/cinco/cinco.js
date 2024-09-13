899a6d014849622e9540eb1890c125407b8c08c5
NelsonJeanN <jean.2013.nelson@gmail.com>
2024-09-13 20:56:44 -0300

    Add files via upload

---
+++ 11-09-2024Part2/cinco/cinco.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")()
+
+let i = 5 
+
+while (i <= 15){
+    console.log(i)
+    i++
+}
---
+++ 11-09-2024Part2/crescente/crescente.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")()
+
+let cont = 1
+for(let i = 1; i <= 10  ; i++){
+
+    console.log(i)
+}
