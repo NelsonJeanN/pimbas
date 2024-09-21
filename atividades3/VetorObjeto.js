ce8e9f08d81e2275fc603f2d839cec2e6514adf5
NelsonJeanN <jean.2013.nelson@gmail.com>
2024-09-21 13:15:49 -0300

    Delete atividades3 directory

--- atividades3/VetorObjeto.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")()
-const nomes = ['Ana', 'João', 'Pedro'];
-
-const objetoNomes = nomes.reduce((conta, nome) => {
-  conta[nome] = nome.length;
-  return conta;
-}, {});
-
-console.log(objetoNomes);
